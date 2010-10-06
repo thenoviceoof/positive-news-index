bb92d9c0e9c72a85b53fcf34a5a2b6330c3351c5
The Novice Oof <beta.entity.k@gmail.com>
2010-10-05 22:19:21 -0400

    started rails project

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,13 @@
+class AdminController < ApplicationController
+  # see all stories, 
+  def index
+  end
+  # see a story edit
+  def view
+  end
+  # update frequencies
+  def edit
+  end
+  def update
+  end
+end
